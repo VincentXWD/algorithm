d5877ff27ff9cab5f938074c6b82b35ac80c8445
VincentXWD <m13853352137@mail.bistu.edu.cn>
2016-06-03 14:19:36 +0800

    16/6/3

---
+++ HDOJ/hdoj2569.cpp
@@ -0,0 +1,86 @@
+/*
+━━━━━┒ギリギリ♂ eye！
+┓┏┓┏┓┃キリキリ♂ mind！
+┛┗┛┗┛┃＼○／
+┓┏┓┏┓┃ /
+┛┗┛┗┛┃ノ)
+┓┏┓┏┓┃
+┛┗┛┗┛┃
+┓┏┓┏┓┃
+┛┗┛┗┛┃
+┓┏┓┏┓┃
+┛┗┛┗┛┃
+┓┏┓┏┓┃
+┃┃┃┃┃┃
+┻┻┻┻┻┻
+*/
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <cstring>
+#include <climits>
+#include <complex>
+#include <fstream>
+#include <cassert>
+#include <cstdio>
+#include <bitset>
+#include <vector>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <ctime>
+#include <set>
+#include <map>
+#include <cmath>
+using namespace std;
+#define fr first
+#define sc second
+#define cl clear
+#define BUG puts("here!!!")
+#define W(a) while(a--)
+#define pb(a) push_back(a)
+#define Rlf(a) scanf("%llf", &a);
+#define Rint(a) scanf("%d", &a)
+#define Rll(a) scanf("%I64d", &a)
+#define Rs(a) scanf("%s", a)
+#define Cin(a) cin >> a
+#define FRead() freopen("in", "r", stdin)
+#define FWrite() freopen("out", "w", stdout)
+#define Rep(i, len) for(int i = 0; i < (len); i++)
+#define For(i, a, len) for(int i = (a); i < (len); i++)
+#define Cls(a) memset((a), 0, sizeof(a))
+#define Clr(a, x) memset((a), (x), sizeof(a))
+#define Full(a) memset((a), 0x7f7f, sizeof(a))
+#define lrt rt << 1
+#define rrt rt << 1 | 1
+#define pi 3.14159265359
+#define RT return
+#define lowbit(x) x & (-x)
+#define onenum(x) __builtin_popcount(x)
+typedef long long LL;
+typedef long double LD;
+typedef unsigned long long ULL;
+typedef pair<int, int> pii;
+typedef pair<string, int> psi;
+typedef map<string, int> msi;
+typedef vector<int> vi;
+typedef vector<LL> vl;
+typedef vector<vl> vvl;
+typedef vector<bool> vb;
+
+const int maxn = 1010;
+int dp[maxn];
+int n;
+
+int main() {
+    // FRead();
+    dp[1] = 3; dp[2] = 9;
+    For(i, 3, 41) dp[i] = 2 * dp[i-1] + dp[i-2];
+    int T;
+    Rint(T);
+    W(T) {
+        Rint(n);
+        printf("%d\n", dp[n]);
+    }
+    RT 0;
+}
